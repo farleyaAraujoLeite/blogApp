ad2cdf2eff9652baf56e4ce4cf114144b94ddc4b
farleyaAraujoLeite <farley2181@gmail.com>
2024-03-30 13:48:46 -0300

    criação da função editar categoria.

--- routes/admin.js
+++ routes/admin.js
@@ -61,5 +61,35 @@ router.post("/categorias/nova", (req, res) => {
   }
 
 })
+//função para editar categorias
+router.get("/categorias/edit/:id", (req, res) => {
+  Categoria.findOne({_id:req.params.id}).then((categoria) => {
+    res.render("admin/editcategoria", {categoria: categoria})
+  }).catch((err) => {
+    req.flash("error_msg","Essa categoria não existe!");
+    res.redirect("/admin/categorias");
+  })
+  
+})
+
+router.post("/categorias/edit", (req, res) => {
+  Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+    categoria.nome = req.body.nome,
+    categoria.slug = req.body.slug
+
+    categoria.save().then(() => {
+      req.flash("success_msg", "Categoria editada com sucesso!");
+      res.redirect("/admin/categorias")
+    }).catch((err) => {
+      req.flash("error_msg", "Erro ao editar a categoria!");
+      res.redirect("/admin/categorias");
+    })
+
+  }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao editar a categoria!")
+    res.redirect("/admin/categorias");
+  })
+})
 
 module.exports = router;
