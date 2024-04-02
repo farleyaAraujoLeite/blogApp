f49f517b0ed94d0fda6f82920e3a0655c401498f
farleyaAraujoLeite <farley2181@gmail.com>
2024-04-01 21:55:12 -0300

    Etapa de criação do formulário para adição de postagens.

--- routes/admin.js
+++ routes/admin.js
@@ -102,4 +102,17 @@ router.post("/categorias/deletar", (req, res) => {
   })
 })
 
+router.get("/postagens", (req, res) => {
+  res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+  Categoria.find().then((categorias) => {
+    res.render("admin/addpostagem", {categorias: categorias})
+  }).catch((err) => {
+    req.flash("Houve um erro ao renderizar as categorias!")
+  })
+  
+})
+
 module.exports = router;
