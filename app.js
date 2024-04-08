01ff4868bcf2182aa3927cde05c96c869ab0d91c
farleyaAraujoLeite <farley2181@gmail.com>
2024-04-08 20:06:32 -0300

    criação da rota de listagem de categorias e da tela de listagem.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
 //importando rotas
 const admin = require("./routes/admin");
 //const mongoose = require("mongoose");
@@ -70,6 +72,34 @@ const admin = require("./routes/admin");
       }
     }).catch((err) => {
       req.flash("Error_msg", "Houve um erro interno!");
+      res.redirect("/");
+    })
+  })
+
+  app.get("/categorias", (req, res) => {
+    Categoria.find().then((categorias) => {
+      res.render("categorias/index", {categorias: categorias});
+    }).catch((err) => {
+      req.flash("error_msg", "Houve um erro ao postar as categorias!");
+      res.render("/");
+    })
+  })
+
+  app.get("/categorias/:slug", (req, res) => {
+    Categoria.findOne({slug: req.params.slug}).then((categoria) => {
+      if(categoria){
+        Postagem.find({categoria: categoria._id}).then((postagens) => {
+          res.render("categorias/postagens", {postagens: postagens, categoria: categoria});
+        }).catch((err) => {
+          req.flash("error_msg", "Houve um erro ao listar os posts!");
+          res.redirect("/");
+        })
+      }else{
+        req.flash("error_msg", "Essa categoria não existe!");
+        res.redirect("/");
+      }
+    }).catch((err) => {
+      req.flash("error_msg", "Houve um erro ao listar a categoria!")
       res.redirect("/");
     })
   })
