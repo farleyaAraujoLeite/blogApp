3974f6c3be88083f04b2902868fcb0a933c4e396
farleyaAraujoLeite <farley2181@gmail.com>
2024-04-10 19:20:56 -0300

    criando registro de usuario e protegendo a senha com o bcrypt.

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,38 +3,74 @@ const router = express.Router();
 const mongoose = require("mongoose");
 require("../models/Usuario");
 const Usuario = mongoose.model("usuarios");
+const bcrypt = require("bcryptjs");
 
 router.get("/registro", (req, res) => {
   res.render("usuarios/registro")
 })
 // Validação de usuario
 router.post("/registro", (req, res) => {
+
   var erros = [];
 
-  if(!req.body.nome ||typeof req.body.nome == undefined || req.body.nome == null) {
-    erros.push({Texto: "Nome inválido!"})
+  if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+    erros.push({texto: "Nome inválido!"})
   }
 
-  if(!req.body.email ||typeof req.body.email == undefined || req.body.email == null) {
-    erros.push({Texto: "Email inválido!"})
+  if(!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+    erros.push({texto: "Email inválido!"})
   }
 
-  if(!req.body.senha ||typeof req.body.senha == undefined || req.body.senha == null) {
-    erros.push({Texto: "Sena inválida!"})
+  if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null) {
+    erros.push({texto: "Sena inválida!"})
   }
 
   if(req.body.senha.length < 4) {
-    erros.push({Texto: "Senha muito curta!"})
+    erros.push({texto: "Senha muito curta!"})
   }
 
   if(req.body.senha != req.body.senha2) {
-    erros.push({Texto: "As senhas são diferentes, tente novamente!"})
+    erros.push({texto: "As senhas são diferentes, tente novamente!"})
   }
-
+// Validação de usuário para saber se já existe no banco de dados
   if(erros.length > 0){
-    res.render("usuarios/registro", {Erros: erros})
+    res.render("usuarios/registro", {erros: erros})
   }else{
-    //próxima aula
+    Usuario.findOne({email: req.body.email}).then((usuario) => {
+      if(usuario){
+        req.flash("error_msg", "Já existe uma conta cadastrada com esse email no nosso sistema!");
+        res.redirect("/usuarios/registro");
+      }else{
+        const novoUsuario = new Usuario({
+          nome: req.body.nome,
+          email: req.body.email,
+          senha: req.body.senha
+        })
+        // encriptando a senha com hash
+        bcrypt.genSalt(10, (erro, salt) => {
+          bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+            if(erro){
+            req.flash("error_msg", "Houve um erro durante o salvamento do usuário!");
+            res.redirect("/")
+            }
+
+            novoUsuario.senha = hash
+            // Salvando novo usuário
+            novoUsuario.save().then(() => {
+              req.flash("success_msg", "Usuario criado com sucesso!");
+              res.redirect("/");
+            }).catch((err) => {
+              req.flash("error_msg", "Houve um erro ao registrar o usuário, tente novamente!");
+              res.redirect("/usuarios/registro");
+            })
+
+          })
+        })
+      }
+    }).catch((err) => {
+      req.flash("error_msg", "Houve um erro interno!");
+      res.redirect("/");
+    })
   }
 })
 
